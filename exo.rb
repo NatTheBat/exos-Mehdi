cb9cad7109ac0de45c74dc626fb7a62dfab2e212
NatTheBat <natacha.agafonov@gmail.com>
2019-07-01 19:21:53 +0200

    first commit

---
+++ exo.rb
@@ -0,0 +1,8 @@
+def concat_and_display amanger, aboire
+	puts 'Servez-moi immédiatement ' + amanger + ' ' + 'et aussi ' + aboire
+end
+
+concat_and_display 'des frites', 'de la bière'
+concat_and_display 'du caviar', 'de la vodka'
+concat_and_display 'une salade', 'de la badoit'
+concat_and_display 'un big mac', 'un lait grenadine'
